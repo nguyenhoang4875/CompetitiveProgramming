1523cc095ef6a0e258ee4df12166eaf287291b1e
nguyenhoang4875 <nguyenhoang4875@gmail.com>
2023-09-18 23:55:40 +0700

    Update template.cpp file

--- Template.cpp
+++ Template.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+
+#include <chrono>
 #define int long long
 
 using namespace std;
@@ -6,17 +8,24 @@ using namespace std;
 int tcs, n;
 
 void solve() {
-
+    cin >> n;
+    cout << "n = " << n << endl;
 }
 
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> tcs;
-    while (tcs--) {
-        solve();
-    }
+    // freopen("../../input.txt", "r", stdin);
+    // freopen("../../output.txt", "w", stdout);
+
+    auto start = chrono::system_clock::now();
+
+    solve();
+
+    auto end = chrono::system_clock::now();
+    cerr << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << "ms" << std::endl;
     return 0;
 }
 
@@ -30,4 +39,4 @@ int32_t main() {
  * vector cause MLE or TLE
  * WRITE STUFF DOWN
  * DON'T GET STUCK ON ONE APPROACH
-*/
+ */
