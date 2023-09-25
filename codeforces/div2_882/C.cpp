3406d3d20bb92dd7afbf5a968ccd7c10885ef7ce
nguyenhoang4875 <nguyenhoang4875@gmail.com>
2023-09-25 21:45:39 +0700

    add solution for C problem in cf div2 882

--- codeforces/div2_882/C.cpp
+++ codeforces/div2_882/C.cpp
@@ -3,23 +3,32 @@
 
 using namespace std;
 
+using vi = vector<int>;
+
 int tcs, n;
 int a[100005];
 
 void solve() {
     cin >> n;
-    for(int i = 1; i <= n; i++) {
-        cin >> a[i];
-    }
-    int sum = 0;
-    int temp = 0;
-    for(int i = 1; i <= n; i++) {
-        temp ^= a[i];
-        sum = max(temp, sum);
-        sum = max(sum, a[i]);
-    }
-    cout << sum << "\n";
+    vector<int> a(n);
+    for (auto &i : a) cin >> i;
 
+    int const max_value = 1 << 8;
+    vector<bool> has_pref(max_value);
+    has_pref[0] = true;
+    int cur_xor = 0;
+    int ans = 0;
+    for (auto i : a) {
+        cur_xor ^= i;
+        for (int pref = 0; pref < max_value; ++pref) {
+            if (has_pref[pref]) {
+                ans = max(ans, pref ^ cur_xor);
+            }
+        }
+        has_pref[cur_xor] = true;
+    }
+
+    cout << ans << '\n';
 }
 
 int32_t main() {
