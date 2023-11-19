e7f5cc0c3876bbef354263c888086388b52c2b68
nguyenhoang4875 <nguyenhoang4875@gmail.com>
2023-11-19 21:59:06 +0700

    update sieve prime number

--- AA_CP_FORM/sieve_prime_number.cpp
+++ AA_CP_FORM/sieve_prime_number.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // print all prime numbers not greater than 'n'
 // O(n*log(log(n)))
-/* 
+/*
 void SieveOfEratosthenes(int n) {
     vector<bool> prime(n + 1, true);
 
@@ -31,14 +31,13 @@ void SieveOfEratosthenes(int n) {
 //////////////////////////////////////////////
 
 vector<int> primeNumber;
-void SieveOfEratosthenes(int n) {
+void sievePrime(int n) {
     vector<bool> prime(n + 1, true);
     for (int p = 2; p * p <= n; p++) {
         if (prime[p] == true) {
             for (int i = p * p; i <= n; i += p) prime[i] = false;
         }
     }
-    // primeNumber.push_back(1);
     for (int p = 2; p <= n; p++)
         if (prime[p]) primeNumber.push_back(p);
 }
@@ -61,28 +60,63 @@ vector<int> getDivisors(int n) {
 // O(sqrt(n))
 vector<int> givePrimeDivisor(int n) {
     vector<int> div;
-    for(int i = 2; i * i <= n; i++) {
-        if(n % i == 0) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
             div.push_back(i);
-            while(n % i == 0) n /= i;
+            while (n % i == 0) n /= i;
         }
     }
-    if(n > 1) div.push_back(n);
+    if (n > 1) div.push_back(n);
     return div;
+}
+
+//**************** Find factors primes number of a number ****************//
+// O(log n)
+vector<int> sieve(1e6 + 1, 0);  // ms: max size
+void primeSieve(int n) {
+    for (int i = 1; i <= n; i++) {
+        sieve[i] = i;
+    }
+
+    for (int i = 2; i <= n; i++) {
+        if (sieve[i] == i) {
+            for (int j = i * i; j <= n; j += i) {
+                if (sieve[j] == j) {
+                    sieve[j] = i;
+                }
+            }
+        }
+    }
+}
+
+vector<int> getFactor(int number) {
+    vector<int> factors;
+    while (number != 1) {
+        factors.push_back(sieve[number]);
+        number /= sieve[number];
+    }
+    return factors;
 }
 
 // Using:
 int32_t main() {
     int n = 60;
-    cout << "Following are the prime numbers smaller than or equal to " << n << endl;
-    SieveOfEratosthenes(n);
-    for(auto e: primeNumber) cout << e << " ";
+    cout << "The prime numbers smaller than or equal to " << n << endl;
+    sievePrime(n);
+    for (auto e : primeNumber) cout << e << " ";
 
     cout << endl;
-    cout << "All prime divisors of " << n << endl;
+    cout << "All prime divisors with O(sqrt n) of " << n << endl;
     vector<int> primeDivisor = givePrimeDivisor(n);
-    for(auto e: primeDivisor) cout << e << " ";
+    for (auto e : primeDivisor) cout << e << " ";
     cout << endl;
+
+    cout << "All prime factors with O(log n) of " << n << endl;
+    primeSieve(n);
+    vector<int> factors = getFactor(n);
+    for (auto e : factors) cout << e << " ";
+    cout << endl;
+
     cout << '\n';
     return 0;
 }
