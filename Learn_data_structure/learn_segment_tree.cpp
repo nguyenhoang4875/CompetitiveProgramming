9f9fd26576839785b78e933c65f7797d42ca7420
nguyenhoang4875 <nguyenhoang4875@gmail.com>
2023-12-17 22:08:51 +0700

    update learn segment tree for range update

--- Learn_data_structure/learn_segment_tree.cpp
+++ Learn_data_structure/learn_segment_tree.cpp
@@ -67,6 +67,89 @@ struct SegmentTree {
     void update(int pos, int newVal) { return update(1, 1, n, pos, newVal); }
 };
 
+// Segment Tree Lazy Propagation
+// range update in O(log n)
+struct SegmentTreeLazy {
+    int n;
+    vector<int> t, lazy;
+
+    SegmentTreeLazy(int _n) {
+        n = _n;
+        t.resize(4 * n, 0);
+        lazy.resize(4 * n, 0);
+    }
+
+    void build(int a[], int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int mid = (tl + tr) / 2;
+            build(a, 2 * v, tl, mid);
+            build(a, 2 * v + 1, mid + 1, tr);
+            t[v] = t[2 * v] + t[2 * v + 1];
+        }
+    }
+
+    int combine(int v1, int v2) { return v1 + v2; }
+
+    int query(int v, int tl, int tr, int l, int r) {
+        // not overlapping case
+        if (tl > r || tr < l) return 0;
+
+        // lazy propagation / clear the lazy update
+        if (lazy[v] != 0) {
+            // pending updates
+            // update segment tree node
+            t[v] += lazy[v] * (tr - tl + 1);
+
+            if (tl != tr) {
+                // propagate the updated value
+                lazy[2 * v] += lazy[v];
+                lazy[2 * v + 1] += lazy[v];
+            }
+            lazy[v] = 0;
+        }
+
+        if (l <= tl and tr <= r) return t[v];
+
+        int mid = (tl + tr) / 2;
+        return combine(query(2 * v, tl, mid, l, r),
+                       query(2 * v + 1, mid + 1, tr, l, r));
+    }
+
+    void update(int v, int tl, int tr, int l, int r, int val) {
+        // not overlapping case
+        if (tl > r || tr < l) return;
+
+        if (lazy[v] != 0) {
+            t[v] += lazy[v] * (tr - tl + 1);
+            if (tl != tr) {
+                lazy[2 * v] += lazy[v];
+                lazy[2 * v + 1] += lazy[v];
+            }
+            lazy[v] = 0;
+        }
+
+        // complete overlapping case
+        if (l <= tl and tr <= r) {
+            t[v] += val * (tr - tl + 1);
+            if (tl != tr) {
+                lazy[2 * v] += val;
+                lazy[2 * v + 1] += val;
+            }
+            return;
+        }
+
+        // partial case
+        int mid = (tl + tr) / 2;
+        update(2 * v, tl, mid, l, r, val);
+        update(2 * v + 1, mid + 1, tr, l, r, val);
+        t[v] = t[2 * v] + t[2 * v + 1];
+    }
+    int query(int l, int r) { return query(1, 1, n, l, r); }
+    void update(int l, int r, int val) { update(1, 1, n, l, r, val); }
+};
+
 // Finding the maximum (similar for minimum)
 struct SegmentTreeMax {
     int n;
