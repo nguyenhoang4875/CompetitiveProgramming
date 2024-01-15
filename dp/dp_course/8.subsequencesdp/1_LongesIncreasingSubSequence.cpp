42e2d3a37ff7cbfc9aed198e26d1991de7337905
hoang5.nguyen <hoang5.nguyen@lge.com>
2024-01-15 10:20:10 +0700

    update long increasing subsequence n log n

--- dp/dp_course/8.subsequencesdp/1_LongesIncreasingSubSequence.cpp
+++ dp/dp_course/8.subsequencesdp/1_LongesIncreasingSubSequence.cpp
@@ -26,6 +26,23 @@ using vvc = vector<vc>;
 
 //*** START CODING ***//
 
+// O(n log n)
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans;
+    ans.push_back(nums[0]);
+
+    for (int i = 1; i < n; i++) {
+        if (nums[i] > ans.back()) {
+            ans.push_back(nums[i]);
+        } else {
+            int low = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+            ans[low] = nums[i];
+        }
+    }
+    return ans.size();
+}
+
 int n;
 
 void solve() {
