8d53a4ea66f8d0296f741c0e9d532eb3b53d9c9a
hoang5.nguyen <hoang5.nguyen@lge.com>
2024-05-20 18:26:23 +0700

    add sol ABC Array Game

--- codejam/2022_final_r2/D_ABCArrayGame.cpp
+++ codejam/2022_final_r2/D_ABCArrayGame.cpp
@@ -1,38 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define int long long
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define F first
-#define S second
 
 using namespace std;
 
-//*** debug(x) ***//
-#define debug(x) cout << "[" << #x << "]" << " : " << (x) << endl
-
-#if 1
-template <class Ch, class Tr, class Container>
-basic_ostream <Ch, Tr> & operator << (basic_ostream <Ch, Tr> & os, Container const& x) {
-    os << "{ ";
-    for(auto& y : x)os << y << ", ";
-    return os << "}";
-}
-
-template <class X, class Y>
-ostream & operator << (ostream & os, pair <X, Y> const& p) {
-    return os << "(" << p.first << ", " << p.second << ")" ;
-}
-#endif
-
-template<typename T>
-void check_here(T x){
-    cout << "------------- " << x << " -------------" << endl;
-}
-
 //*** define ***//
-#define For(_i,_a,_b) for(int _i = (_a); _i < (_b); _i++)
-#define Fori(_i,_a,_b) for(int _i = (_a); _i <= (_b); _i++)
-#define Fore(it,x) for(auto it = x.begin(); it != x.end(); ++it)
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sz(a) (int)(a).size()
+#define el '\n'
+#define F first
+#define S second
+#define For(i, a, b) for (int i = a; i <= (int)b; i++)
+#define Ford(i, a, b) for (int i = a; i >= (int)b; i--)
+#define Fore(it, x) for (auto it = x.begin(); it != x.end(); ++it)
 
 //*** custom using ***//
 using vb = vector<bool>;
@@ -46,57 +26,73 @@ using vii = vector<pii>;
 
 //*** START CODING ***//
 
-const int oo = 1e18, mod = 1e9 + 7;
-const int ms = 1e5 + 5;
-int n, a, b, c;
-int v[ms];
-int preSum[ms];
+const int oo = 2e18, mod = 1e9 + 7;
+const int ms = 2e5 + 5;
+int n, m = 3;
+int a[ms], ps[ms];
+int d[10];
+int f[ms][5][2];
+
+int getSum(int l, int r) { return ps[r] - ps[l - 1]; }
 
 void solve() {
     cin >> n;
-    cin >>  a >> b >> c;
-    // debug(n);
-    Fori(i, 1, n) cin >> v[i];
-    Fori(i, 1, n) preSum[i] = preSum[i - 1] + v[i];
+    for (int i = 1; i <= 3; i++) cin >> d[i];
 
-    // Fori(i, 1, n) cout << v[i] << " ";
-    // cout << endl;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) ps[i] = ps[i - 1] + a[i];
 
-    Fori(i, 1, n) cout << preSum[i] << " ";
-    cout << endl;
-
-
-    vi tt = {0, 1, 2};
-    vi s = {a, b, c};
-    int ans = -2*oo;
-    int total = a + b + c;
+    // vi p = {1, 2, 3};
+    sort(d + 1, d + 3 + 1);
+    int ans = -oo;
     do {
-        a = s[tt[0]];
-        b = s[tt[1]];
-        c = s[tt[2]];
-        cout << a << b << c << endl;
-        int sa, sb, sc;
-        for(int i = 1; i <= n - total + 1; i++) {
-            sa = preSum[i + a - 1] - preSum[i - 1];
-            sb = preSum[i + a + b - 1] - preSum[i + a - 1];
-            sc = preSum[i + a + b + c - 1] - preSum[i + a + b - 1];
-            cout << sa << " " << sb  << " " << sc << "\n";
-            ans = max(ans, sa * sb * sc);
+        // set init value
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= 3; j++) {
+                f[i][j][0] = oo;
+                f[i][j][1] = -oo;
+            }
         }
 
+        // base case
+        for (int i = 0; i <= n; i++) {
+            f[i][0][0] = 1;
+            f[i][0][1] = 1;
+        }
 
-    } while(next_permutation(tt.begin(), tt.end()));
-    cout << ans << '\n';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 3; j++) {
+                // not taken
+                f[i][j][1] = f[i - 1][j][1];
+                f[i][j][0] = f[i - 1][j][0];
 
+                /// taken postion i for j
+                int len = d[j];
+                if (i - len >= 0) {
+                    if (f[i - len][j - 1][1] != -oo) {
+                        f[i][j][1] = max(f[i][j][1], f[i - len][j - 1][1] * getSum(i - len + 1, i));
+                        f[i][j][0] = min(f[i][j][0], f[i - len][j - 1][1] * getSum(i - len + 1, i));
+                    }
+                    if (f[i - len][j - 1][0] != oo) {
+                        f[i][j][1] = max(f[i][j][1], f[i - len][j - 1][0] * getSum(i - len + 1, i));
+                        f[i][j][0] = min(f[i][j][0], f[i - len][j - 1][0] * getSum(i - len + 1, i));
+                    }
+                }
+            }
+        }
+        ans = max(ans, f[n][3][1]);
+
+    } while (next_permutation(d + 1, d + 4));
+    cout << ans << el;
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tcs;
+    int tcs = 1;
     cin >> tcs;
-    while(tcs--) {
+    while (tcs--) {
         solve();
     }
     return 0;
