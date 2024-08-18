5983b6dcc638f9ab4e936f73259273cdebbd352e
nguyenhoang4875 <nguyenhoang4875@gmail.com>
2024-08-18 23:48:28 +0700

    update debug code for vector<bool>

--- AA_CP_FORM/test_debug.cpp
+++ AA_CP_FORM/test_debug.cpp
@@ -1,12 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 //******************** define *********************//
 #define int long long
-#define FOR(_i,_a,_b) for(int _i = (_a); _i < (_b); _i++)
-#define FORI(_i,_a,_b) for(int _i = (_a); _i <= (_b); _i++)
-#define FORE(it,x) for(auto it = x.begin(); it != x.end(); ++it)
+#define FOR(_i, _a, _b) for (int _i = (_a); _i < (_b); _i++)
+#define FORI(_i, _a, _b) for (int _i = (_a); _i <= (_b); _i++)
+#define FORE(it, x) for (auto it = x.begin(); it != x.end(); ++it)
 
 //******************** typedef *********************//
 // typedef long long int lli;
@@ -17,27 +17,27 @@ typedef vector<pair<int, int>> vii;
 //******************* debug(x) *********************//
 //****https://codeforces.com/blog/entry/68920****//
 
-//#define watch(x) cout << "[" << #x << "]" << " : " << (x) << endl
-//#define getName(var) #var
+// #define watch(x) cout << "[" << #x << "]" << " : " << (x) << endl
+// #define getName(var) #var
 #define debug(x) cout << "[" << #x << "]" << " : " << (x) << endl
 
 #if 1
 template <class Ch, class Tr, class Container>
-basic_ostream <Ch, Tr> & operator << (basic_ostream <Ch, Tr> & os, Container const& x) {
-	os << "{ ";
-	for(auto& y : x)os << y << ", ";
-	return os << "}";
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& os, Container const& x) {
+    os << "{ ";
+    for (const auto& y : x) os << y << ", ";
+    return os << "}";
 }
 
 template <class X, class Y>
-ostream & operator << (ostream & os, pair <X, Y> const& p) {
-	return os << "(" << p.first << ", " << p.second << ")" ;
+ostream& operator<<(ostream& os, pair<X, Y> const& p) {
+    return os << "(" << p.first << ", " << p.second << ")";
 }
 #endif
 
-template<typename T>
-void check_here(T x){
-	cout << "------------- " << x << " -------------" << endl;
+template <typename T>
+void check_here(T x) {
+    cout << "------------- " << x << " -------------" << endl;
 }
 
 //****************** START CODING **********************//
@@ -45,19 +45,21 @@ void check_here(T x){
 int tcs, n;
 
 void solve() {
-   map <int, int> m{ {10, 2}, {11, -2}, {-6, 4} };
+    map<int, int> m{{10, 2}, {11, -2}, {-6, 4}};
     float i = 10.019;
     string s = "this is a string";
-    unordered_map <int, int> um{ {10, 2}, {11, -2}, {-6, 4} };
-    vector <string> vs{"heh", "hah"};
-    vector <vector <int>> vvi{{1, 2}, {5, 6, 8}, {}};
+    unordered_map<int, int> um{{10, 2}, {11, -2}, {-6, 4}};
+    vector<string> vs{"heh", "hah"};
+    vector<vector<int>> vvi{{1, 2}, {5, 6, 8}, {}};
     vector<int> vi{1, 3, 5, 7, 8};
     vector<char> vc{'a', 'c', 'f'};
-    vector<pair<int, string>> vpi{ {10, "muoi"}, {11, "muoi mot"}, {-6, "bon"}, {}, {12, "muoi hai"} };
-    set <string> ss{"heh", "hah"};
-    set <set <int>> ssi{{1, 2}, {5, 6, 8}, {}};
+    vector<pair<int, string>> vpi{
+        {10, "muoi"}, {11, "muoi mot"}, {-6, "bon"}, {}, {12, "muoi hai"}};
+    set<string> ss{"heh", "hah"};
+    set<set<int>> ssi{{1, 2}, {5, 6, 8}, {}};
     const char* cs = "hah";
     char c = 's';
+    vector<bool> vb = {1, 1, 1, 0};
 
     debug(m);
     debug(i);
@@ -72,16 +74,17 @@ void solve() {
     debug(ssi);
     debug(cs);
     debug(c);
-	check_here(123);
+    debug(vb);
+    check_here(123);
 }
 
 int32_t main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	// cin >> tcs;
-	tcs = 1;
-	while(tcs--) {
-		solve();	
-	}
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // cin >> tcs;
+    tcs = 1;
+    while (tcs--) {
+        solve();
+    }
+    return 0;
 }
