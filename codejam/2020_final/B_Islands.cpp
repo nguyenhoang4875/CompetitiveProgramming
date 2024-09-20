ea8eba7e50192f2695153daf4044e7ca415c83bb
nguyenhoang4875 <nguyenhoang4875@gmail.com>
2024-09-20 21:31:27 +0700

    add island 2020 final round

--- codejam/2020_final/B_Islands.cpp
+++ codejam/2020_final/B_Islands.cpp
@@ -3,16 +3,18 @@
 
 using namespace std;
 
+//*** define ***//
 #define pb push_back
-#define all(x) x.begin(), x.end()
+#define all(x) (x).begin(), (x).end()
 #define sz(a) (int)(a).size()
 #define el '\n'
 #define F first
 #define S second
-#define For(i, a, b) for (int i = a; i <= (int)b; i++)
-#define Ford(i, a, b) for (int i = a; i >= (int)b; i--)
-#define Fore(it, x) for (auto it = x.begin(); it != x.end(); ++it)
+#define For(i, a, b) for (int i = (a); i <= (int)(b); i++)
+#define Rof(i, a, b) for (int i = (a); i >= (int)(b); i--)
+#define Fore(it, x) for (auto it = (x).begin(); it != (x).end(); ++it)
 
+//*** custom using ***//
 using vb = vector<bool>;
 using vvb = vector<vb>;
 using vc = vector<char>;
@@ -23,77 +25,72 @@ using pii = pair<int, int>;
 using vii = vector<pii>;
 
 //*** START CODING ***//
+struct Dsu {
+    int n;
+    vector<int> parent, size;
 
-//*** debug(x) ***//
-#if 1
-#define debug(x)             \
-    cout << "[" << #x << "]" \
-         << " : " << (x) << endl
+    Dsu() {};
+    Dsu(int _n) {
+        n = _n;
+        parent = vector<int>(n + 1);
+        size = vector<int>(n + 1);
+        for (int v = 1; v <= n; v++) makeSet(v);
+    }
 
-template <class Ch, class Tr, class Container>
-basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& os, Container const& x) {
-    os << "{ ";
-    for (auto& y : x) os << y << ", ";
-    return os << "}";
-}
+    void makeSet(int v) {
+        parent[v] = v;
+        size[v] = 1;  // init size = 1;
+    }
 
-template <class X, class Y>
-ostream& operator<<(ostream& os, pair<X, Y> const& p) {
-    return os << "(" << p.first << ", " << p.second << ")";
-}
+    int findSet(int v) { return parent[v] = (v == parent[v] ? v : findSet(parent[v])); }
 
-template <typename T>
-void check_here(T x) {
-    cout << "------------- " << x << " -------------" << endl;
-}
-#endif
+    bool unionSet(int a, int b) {
+        a = findSet(a);
+        b = findSet(b);
+        if (a == b) return false;
 
-const int oo = 1e18, mod = 1e9 + 7;
-const int ms = 1e4 + 5;
-int n, s, a, b;
-int e[ms];
+        if (size[a] < size[b]) swap(a, b);  // set a is root of the bigger size of tree
+        parent[b] = a;
+        size[a] += size[b];  // update the new size of new the merge tree
+        return true;
+    }
+};
+
+const long long oo = 2e18, mod = 1e9 + 7;
+const int ms = 2e5 + 5;
 
 void solve() {
-    cin >> n >> s >> a >> b;
-    int sqn = n * n;
-    e[1] = s % sqn;
+    int n, seed, a, b;
+    cin >> n >> seed >> a >> b;
 
-    set<pii> si;
-    map<pii, int> mi;
-    int x = e[1] / n, y = e[1] % n;
-    if (x != y) {
-        si.insert({x, y});
+    Dsu dsu(n);
+    int e = seed % (n * n);
+    int x, y;
+    int node = 1;
+    set<pii> st;
+    int m = 0;
+    while (node < n) {
+        x = e / n;
+        y = e % n;
+        m++;
+        if (dsu.unionSet(x, y)) {
+            node++;
+        }
+        if (st.count({x, y})) {
+            break;
+        }
+        st.insert({x, y});
+        e = (e * a + b) % (n * n);
     }
-
-    int i = 1;
-    while (i <= n * n) {
-        i++;
-        e[i] = (e[i - 1] * a % sqn + b % sqn) % sqn;
-        x = e[i] / n;
-        y = e[i] % n;
-        // debug(x);
-        // debug(y);
-        if (x == y) continue;
-        if (si.count({x, y})) {
-            cout << 0 << el;
-            return;
-        }
-        if (!si.count({y, x})) {
-            si.insert({x, y});
-        }
-        // debug(sz(si));
-        if (si.size() == n - 1) {
-            cout << i << el;
-            return;
-        }
-    }
+    if (node < n) m = 0;
+    cout << m << el;
 }
 
 int32_t main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tcs;
+    int tcs = 1;
     cin >> tcs;
     while (tcs--) {
         solve();
