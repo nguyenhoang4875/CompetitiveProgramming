0e76a4e78d6521a1a7fda6dd992b43e820592798
nguyenhoang4875 <nguyenhoang4875@gmail.com>
2025-03-24 22:06:33 +0700

    add get binary and submasks

--- dp/dp_course/10.bitmaskdp/1_Bitmanipulation.cpp
+++ dp/dp_course/10.bitmaskdp/1_Bitmanipulation.cpp
@@ -1,18 +1,39 @@
 #include <bits/stdc++.h>
 #define int long long
-#define pb push_back
+#define pb push
+#define el '\n'
 
 using namespace std;
 /**
  * Bit Manipulations
  */
 
-int n;
-
 int setBit(int n, int pos) { return n |= (1 << pos); }
 int clearBit(int n, int pos) { return n &= ~(1 << pos); }
 int getBit(int n, int pos) { return (n >> pos) & 1; }
 int toggleBit(int n, int pos) { return n ^= (1 << pos); }
+
+string getBin(int n) {
+    string ans = "";
+    while (n) {
+        if (n & 1) ans.push_back('1');
+        else ans.push_back('0');
+        n >>= 1;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+vector<int> getSubSetMask(int mask) {
+    vector<int> ans;
+    int subMask = mask;
+    while(subMask) {
+        ans.push_back(subMask);
+        subMask = (subMask - 1) & mask;
+    }
+    ans.push_back(0);
+    return ans;
+}
 
 void solve() {
     int n = 9;                        // 1001
@@ -21,6 +42,12 @@ void solve() {
     cout << setBit(n, 1) << '\n';     // 1011: 11
     cout << clearBit(n, 3) << '\n';   // 0001: 1
     cout << toggleBit(n, 2) << '\n';  // 1101: 13
+
+    cout << getBin(n) << '\n';
+
+    vector<int> subMasks = getSubSetMask(n);
+    for(auto e: subMasks) cout << e << " ";
+    cout << '\n';
 }
 
 int32_t main() {
