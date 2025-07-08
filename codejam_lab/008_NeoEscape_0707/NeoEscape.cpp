b978d462c5f355e00e31517f952e004448267324
hoang5.nguyen <hoang5.nguyen@lge.com>
2025-07-08 10:23:38 +0700

    add NeoEscape Problem

---
+++ codejam_lab/008_NeoEscape_0707/NeoEscape.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define all1(x) (x).begin() + 1, (x).end()
+#define el '\n'
+#define F first
+#define S second
+#define Rep(i, n) for (int i = 0; i < (int)n; ++i)
+#define For(i, a, b) for (int i = (a); i <= (int)b; ++i)
+#define Rof(i, b, a) for (int i = (b); i >= (int)a; --i)
+#define Fore(i, v) for (auto i = (v).begin(); i != (v).end(); ++i)
+
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+
+//*** START CODING ***//
+
+const long long oo = 2e18, mod = 1e9 + 7;
+const int ms = 2e5 + 5;
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a;
+    a.reserve(n);
+    a.pb(-1);
+    For(i, 1, n) {
+        int x;
+        cin >> x;
+        if (a.back() != x) a.pb(x);
+    }
+    a.pb(-1);
+    n = a.size();
+    int cnt = 0;
+    For(i, 1, n - 2) {
+        if (a[i - 1] < a[i] and a[i] > a[i + 1]) {
+            cnt++;
+        }
+    }
+    cout << cnt << el;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tcs = 1;
+    cin >> tcs;
+    while (tcs--) {
+        solve();
+    }
+    return 0;
+}
