1ec3493abbc5f0a2abb256c3ed3fc94bb34a3a45
hoang5.nguyen <hoang5.nguyen@lge.com>
2025-07-09 18:10:13 +0700

    add FillMatrix problem

---
+++ codejam_lab/012_FillMatrix_0709/FillMatrix.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define all1(x) (x).begin() + 1, (x).end()
+#define el '\n'
+#define F first
+#define S second
+#define Rep(i, n) for (int i = 0; i < (int)n; ++i)
+#define For(i, a, b) for (int i = (a); i <= (int)b; ++i)
+#define Rof(i, b, a) for (int i = (b); i >= (int)a; --i)
+#define Fore(i, v) for (auto i = (v).begin(); i != (v).end(); ++i)
+
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+
+//*** START CODING ***//
+
+const long long oo = 2e18, mod = 1e9 + 7;
+const int ms = 2e5 + 5;
+
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    vi a(k);
+    Rep(i, k) a[i] = i + 1;
+    Rep(i, n) {
+        Rep(j, m) {
+            int pos = (m % k == 0) ? i : i * m;
+            pos = (pos + j) % k;
+            cout << a[(pos) % k] << " ";
+        }
+        cout << el;
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tcs = 1;
+    cin >> tcs;
+    while (tcs--) {
+        solve();
+    }
+    return 0;
+}
