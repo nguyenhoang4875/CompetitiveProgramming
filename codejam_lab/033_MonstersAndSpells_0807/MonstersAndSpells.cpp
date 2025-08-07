475e5094a91ff859ac84eb8981ef24d7ea6f3e4c
hoang5.nguyen <hoang5.nguyen@lge.com>
2025-08-07 17:28:50 +0700

    add MonsterAndSpells

---
+++ codejam_lab/033_MonstersAndSpells_0807/MonstersAndSpells.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+//*** debug(x) ***//
+#define DEBUG 1
+
+#if DEBUG
+#define del cout << '\n'
+#define debug(...) _debug(#__VA_ARGS__, __VA_ARGS__)
+template <class X, class Y>
+ostream& operator<<(ostream& os, pair<X, Y> const& p) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+template <class Ch, class Tr, class Container>
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& os, Container const& x) {
+    int i = 0, n = (int)distance(x.begin(), x.end());
+    os << "{ ";
+    for (const auto& y : x) os << y << (++i < n ? ", " : "");
+    return os << " }";
+}
+template <typename... Args>
+void _debug(const char* names, Args&&... args) {
+    string_view s(names);
+    cout << "{ ";
+    size_t i = 0, cnt = 0, n = sizeof...(args);
+    auto next = [&]() {
+        while (i < s.size() && (s[i] == ' ' || s[i] == ',')) ++i;
+        size_t st = i;
+        while (i < s.size() && s[i] != ',') ++i;
+        return s.substr(st, i - st);
+    };
+    ((cout << next() << ": " << args << (++cnt < n ? ", " : "")), ...);
+    cout << " }" << '\n';
+}
+#else
+#define del
+#define debug(...)
+#endif
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define all1(x) (x).begin() + 1, (x).end()
+#define sz(x) static_cast<int>((x).size())
+#define el '\n'
+#define F first
+#define S second
+#define Rep(i, n) for (int i = 0; i < (int)n; ++i)
+#define For(i, a, b) for (int i = (a); i <= (int)b; ++i)
+#define Rof(i, b, a) for (int i = (b); i >= (int)a; --i)
+#define Fore(i, v) for (auto i = (v).begin(); i != (v).end(); ++i)
+
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+
+//*** START CODING ***//
+
+const long long oo = 2e18, mod = 1e9 + 7;
+const int ms = 2e5 + 5;
+
+void solve() {
+    int n;
+    cin >> n;
+    vi k(n + 1);
+    For(i, 1, n) cin >> k[i];
+    vi h(n + 1);
+    For(i, 1, n) cin >> h[i];
+
+    int ans = 0;
+    int cur_end = n;
+    Rof(i, n, 1) {
+        int dk = k[i] - k[i - 1];
+        if (dk >= h[i]) {
+            int len = h[i] + k[cur_end] - k[i];
+            ans += len * (len + 1) / 2;
+            cur_end = i - 1;
+        } else {
+            h[i - 1] = max(h[i - 1], h[i] - dk);
+        }
+    }
+    cout << ans << el;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tcs = 1;
+    cin >> tcs;
+    while (tcs--) {
+        solve();
+    }
+    return 0;
+}
