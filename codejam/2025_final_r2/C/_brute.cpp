f69276bfa396a7f4074fc19605837443ff22b765
hoang5.nguyen <hoang5.nguyen@lge.com>
2025-12-04 10:50:41 +0700

    add 2025 final round 2: A, B, C, D

---
+++ codejam/2025_final_r2/C/_brute.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+//*** debug(x) ***//
+#define DEBUG 1
+
+#if DEBUG
+#define del cout << '\n'
+#define debug(...) _debug(#__VA_ARGS__, __VA_ARGS__)
+template <class X, class Y>
+ostream& operator<<(ostream& os, pair<X, Y> const& p) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+template <class Ch, class Tr, class Container>
+basic_ostream<Ch, Tr>& operator<<(basic_ostream<Ch, Tr>& os, Container const& x) {
+    int i = 0, n = (int)distance(x.begin(), x.end());
+    os << "{ ";
+    for (const auto& y : x) os << y << (++i < n ? ", " : "");
+    return os << " }";
+}
+template <typename... Args>
+void _debug(const char* s, Args&&... args) {
+    cout << "{ ";
+    size_t i = 0, cnt = 0, n = sizeof...(args);
+    auto next = [&]() {
+        while (s[i] && (s[i] == ' ' || s[i] == ',')) ++i;
+        size_t st = i;
+        while (s[i] && s[i] != ',') ++i;
+        return string(s + st, i - st);
+    };
+    auto dummy = {(cout << next() << ": " << args << (++cnt < n ? ", " : ""), 0)...};
+    (void)dummy;
+    cout << " }" << '\n';
+}
+#else
+#define del
+#define debug(...)
+#endif
+
+#define pb push_back
+#define LSOne(x) ((x) & -(x))
+#define all(x) (x).begin(), (x).end()
+#define all1(x) (x).begin() + 1, (x).end()
+#define sz(x) static_cast<int>((x).size())
+#define el '\n'
+#define F first
+#define S second
+#define Rep(i, n) for (int i = 0; i < (int)n; ++i)
+#define For(i, a, b) for (int i = (a); i <= (int)b; ++i)
+#define Rof(i, b, a) for (int i = (b); i >= (int)a; --i)
+#define Fore(i, v) for (auto i = (v).begin(); i != (v).end(); ++i)
+
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+
+//*** START CODING ***//
+
+const long long oo = 2e18, mod = 1e9 + 7;
+const int ms = 2e5 + 5;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vvi a(n, vi(m));
+    Rep(i, n) Rep(j, m) cin >> a[i][j];
+
+    int cnt = 0;
+    for (int r1 = 0; r1 < n; ++r1) {
+        for (int r2 = r1 + 1; r2 < n; ++r2) {
+            for (int c1 = 0; c1 < m; ++c1) {
+                For(c2, c1 + 1, m - 1) {
+                    int sum = a[r1][c1] + a[r1][c2] + a[r2][c1] + a[r2][c2];
+                    if (sum == 20) ++cnt;
+                }
+            }
+        }
+    }
+    cout << cnt << el;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+    return 0;
+}
